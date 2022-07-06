2e0de81bdfb36e812241e66d5cc0e51e98aaec98
turner11 <avi.turner111@gmail.com>
2022-07-06 10:02:51 +0300

    Create example_Microsoft.PowerShell_profile.ps1

---
+++ example_Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,43 @@
+## OH MY POSH settings
+Set-PoshPrompt -Theme agnosterplus
+$env:POSH_GIT_ENABLED=$true
+Import-Module posh-git
+Import-Module oh-my-posh
+
+## Make the terminal beutifull & productive
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+
+## History search
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadlineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -PredictionSource History 
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+
+# Add bin to PATH
+$env:Path += "; $env:UserProfile\\.local\\bin"
+
+New-Alias -Name l -value "ls"
+New-Alias -Name cc -value "rich"
+New-Alias -Name onelogin -value cloud-sso
+
+# shortcut for which python
+function wp()
+{
+	python -c 'import sys; print(sys.executable)'
+}
+
+
+# make a nice version of cat utilizing rich
+function ccc([string]$File)
+{
+    C:\\Users\\avit\\.local\\bin\\rich.exe -n -g "$File"
+}
+
+function ecr()
+{
+    aws ecr get-login-password | docker login --username AWS --password-stdin <YOUR ECR REGISTRY>
+}
