605b97a46e89f5a2aee07371e7ad5161380e9a90
turner11 <avi.turner111@gmail.com>
2025-09-30 16:36:00 +0300

    Add Streamlit app template

---
+++ streamlit_app_live_template.py
@@ -0,0 +1,70 @@
+# Insert the following to pycharm live templates
+import dataclasses
+
+import streamlit as st
+import logging
+import coloredlogs
+import sys
+
+
+@dataclasses.dataclass
+class Inputs:
+    some_value: str = 'hello world!'
+
+
+def get_class_from_url(class_type: type[dataclasses.dataclass()] = None):
+    query_params = st.query_params
+    dummy = class_type()
+    data_dict = {**dummy.__dict__}
+    data_dict.update({k: v for k, v in query_params.items() if k in data_dict})
+    instance = class_type(**data_dict)
+    return instance
+
+
+def set_url_from_class(instance):
+    instance_dict = dataclasses.asdict(instance)
+    valid_keys = {}
+    for key, value in instance_dict.items():
+        if key.startswith('_'):
+            continue
+        try:
+            bool(value)  # Check if value can be converted to a boolean
+            valid_keys[key] = value
+        except ValueError:
+            continue
+    d = {k: v for k, v in valid_keys.items() if v not in (0, None, '')}
+    st.query_params.update(d)
+
+
+def get_inputs():
+    inputs = get_class_from_url(Inputs)
+
+    set_url_from_class(inputs)
+    return inputs
+
+
+def app(inputs: Inputs):
+    st.subheader(inputs)
+
+
+def main():
+    logger = logging.getLogger(__name__)
+
+    st.set_page_config(page_title="My app",
+                       page_icon="🧪",
+                       layout="wide",
+                       initial_sidebar_state="expanded",
+                       )
+
+    logger_format = '%(asctime)s [%(name)s] %(module)s::%(funcName)s %(levelname)s - %(message)s'
+    coloredlogs.install(level='CRITICAL', fmt=logger_format)
+    coloredlogs.install(level='DEBUG', fmt=logger_format, logger=logger, stream=sys.stdout, isatty=True)
+
+    with st.sidebar:
+        inputs = get_inputs()
+
+    app(inputs)
+
+
+if __name__ == '__main__':
+    main()
